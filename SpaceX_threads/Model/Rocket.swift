2ea0c985cafab621e240d72d5d2d3bc70d9f35ff
Adriano Leal Ramos <ramos.adrianoleal@gmail.com>
2019-08-27 17:10:22 -0300

    images added to the model

--- SpaceX_threads/Model/Rocket.swift
+++ SpaceX_threads/Model/Rocket.swift
@@ -34,29 +34,11 @@ struct RocketElement: Codable {
     }
 }
 
-
 struct Links: Codable {
-    let missionPatch, missionPatchSmall: String?
-    let redditCampaign: String?
-    let redditLaunch: String?
-    let redditRecovery, redditMedia: String?
-    let presskit: String?
-    let articleLink: String?
-    let wikipedia, videoLink: String?
-    let youtubeID: String?
+    let flickrImages: [String]
     
     enum CodingKeys: String, CodingKey {
-        case missionPatch = "mission_patch"
-        case missionPatchSmall = "mission_patch_small"
-        case redditCampaign = "reddit_campaign"
-        case redditLaunch = "reddit_launch"
-        case redditRecovery = "reddit_recovery"
-        case redditMedia = "reddit_media"
-        case presskit
-        case articleLink = "article_link"
-        case wikipedia
-        case videoLink = "video_link"
-        case youtubeID = "youtube_id"
+        case flickrImages = "flickr_images"
     }
 }
 
