382a16003d63e6ce90e5973030d2f49a518b6739
Luiz Guerra <luizpedrofg@gmail.com>
2019-08-28 18:16:20 -0300

    Created front-basics

---
+++ SpaceX_threads/View/Extensions/UIButtonExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIButtonExtension.swift
+//  SpaceX_threads
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 28/08/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func setAppearance () {
+        self.layer.cornerRadius = 8
+        self.layer.borderColor = #colorLiteral(red: 0.1529411765, green: 0.1529411765, blue: 0.1529411765, alpha: 1)
+        self.layer.borderWidth = 1
+    }
+    
+}
---
+++ SpaceX_threads/View/Extensions/UIViewExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewExtensions.swift
+//  SpaceX_threads
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 28/08/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func setRoundCorners (ammount: CGFloat) {
+        self.layer.cornerRadius = ammount
+    }
+    
+}
